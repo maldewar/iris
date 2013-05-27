828406abfedb428900d21c294e3e0a1dcbce4b1d
maldewar <valdemar@gmail.com>
2013-05-26 22:09:19 -0700

    Implementation to handle random and linear step mode.

--- src/lib/zone/Line3DZone.js
+++ src/lib/zone/Line3DZone.js
@@ -2,15 +2,24 @@ IRIS.Line3DZone = IRIS.Vector3Zone.extend({
     init: function(opts) {
         opts = IRIS._setterUndef(opts, {});
         this._super(opts);
-        this._start = new IRIS.Vector3(this.minX, this.minY, this.minZ);
-        this._end = new IRIS.Vector3(this.maxX, this.maxY, this.maxZ);
+        this.toX = IRIS._setterUndef(opts.toX,1);
+        this.toY = IRIS._setterUndef(opts.toY,1);
+        this.toZ = IRIS._setterUndef(opts.toZ,1);
+        this._start = new IRIS.Vector3(this.x, this.y, this.z);
+        this._end = new IRIS.Vector3(this.toX, this.toY, this.toZ);
         this._len = this._end.clone().sub(this._start);
+        this._stepVector = 0;
     },
     getStep: function() {
         var len = this._len.clone();
-        len.mul(Math.random());
+        if (this.pattern == IRIS.ZONE_PATTERN_RANDOM)
+            len.mul(Math.random());
+        else {
+            len.mul(this._stepVector)
+            this._stepVector += this.step;
+        }
         return len.add(this._start).mul(this.scale);
     }
 });
 
-IRIS.registerZone(IRIS.Line3DZone, 'line3D');
+IRIS.registerZone(IRIS.Line3DZone, IRIS.ZONE_TYPE_LINE_3D);
