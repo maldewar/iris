d6a31650159e92892ccc147cc59b556c54a05bd9
maldewar <valdemar@gmail.com>
2013-06-03 19:28:39 -0700

    Dummy code removed

--- src/lib/scene.js
+++ src/lib/scene.js
@@ -10,11 +10,6 @@ IRIS.Scene = function(opts){
     this.ctrl = {
         asset:{}
     };
-
-    //TODO remove misplaced initializer
-    //this.zone = new IRIS.Line3DZone({scale:100, pattern: IRIS.ZONE_PATTERN_LINEAL, step: 0.5});
-    //this.zone = new IRIS.RectangleZone({scale:100, pattern: IRIS.ZONE_PATTERN_LINEAL, step: 0.5, plane:'xz'});
-    this.zone = new IRIS.RectangleZone({scale:100, step: 0.5, plane:'xz'});
 };
 
 IRIS.Scene.prototype = {
@@ -26,12 +21,6 @@ IRIS.Scene.prototype = {
             return false;
     },
     _addAsset: function(oAsset) {
-        // Apply initializer
-        //TODO Handle nicely initializers TODO
-        var v = this.zone.getStep();
-        oAsset.object.position.x = v.x;
-        oAsset.object.position.y = v.y;
-        oAsset.object.position.z = v.z;
         this.addAsset(oAsset);
     },
     _removeAsset: function() {
