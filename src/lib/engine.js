173d83cd9de161c4e349fc99c57e5cd8464a12c0
maldewar <valdemar@gmail.com>
2013-06-03 19:27:56 -0700

    Engine now uses the modifierProvider.

--- src/lib/engine.js
+++ src/lib/engine.js
@@ -2,8 +2,10 @@ IRIS.Engine = Class.extend({
     init: function(opts) {
         this.isRunning = false;
         //this.scene = opts.scene;
-        this.assetProvider = this._getAssetProviderInstance(this.assetProvider, IRIS.ui.asset);
-
+        this.assetProvider = this._getAssetProviderInstance(this.assetProvider,
+                                IRIS.ui.asset); //TODO pass opts as second param.
+        this.modifierProvider = this._getModifierProviderInstance(this.modifierProvider,
+                                IRIS.ui.modifier); //TODO pass opts as second param.
         /*Functions to be defined by each Engine specific implementation*/
         //this.getScene = opts.getScene; //Returns an instance of a scene the engine will work with.
         //this.createAsset = opts.createAsset; //Must return a valid Asset instance.
@@ -22,6 +24,7 @@ IRIS.Engine = Class.extend({
     },
     _createAsset: function(index, data, oEntity) {
         var oAsset = this.assetProvider.getAsset(index, data, oEntity);
+        this.modifierProvider.apply(oAsset,oAsset.createModifier);
         this.populateAsset(oAsset);
         this.scene._addAsset(oAsset);
     },
@@ -32,6 +35,12 @@ IRIS.Engine = Class.extend({
     _getAssetProviderInstance: function (id, opts) {
         if (typeof IRIS.ctrl.assetProvider[id] !== 'undefined')
             return new IRIS.ctrl.assetProvider[id](opts);
+        else
+            return false;
+    },
+    _getModifierProviderInstance: function(id, opts) {
+        if (typeof IRIS.ctrl.modifierProvider[id] !== 'undefined')
+            return new IRIS.ctrl.modifierProvider[id](opts);
         else
             return false;
     }
