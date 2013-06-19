7a5ffc6110f8c6018b6066bc90a811af8bd1ab72
maldewar <valdemar@gmail.com>
2013-06-18 21:16:35 -0700

    Now the plane option doesn't mess with the x,y,z params.

--- src/lib/zone/CircleZone.js
+++ src/lib/zone/CircleZone.js
@@ -5,14 +5,25 @@ IRIS.CircleZone = IRIS.Vector2Zone.extend({
         this.radius = IRIS._setterUndef(opts.radius,1);
         this._stepVector = 0;
     },
-    getStep: function() {
+    getStep: function() { //TODO: call the parent getStep first by default and the onStepEvent there.
         if (this.pattern == IRIS.ZONE_PATTERN_RANDOM)
             var theta = Math.PI * 2 * Math.random();
         else {
             var theta = this._stepVector;
             this._stepVector += this.step;
         }
-        return new IRIS.Vector3(this.radius * Math.cos(theta) * this.scale + this.x, this.radius * Math.sin(theta) * this.scale + this.y, 0);
+        var x = this.radius * Math.cos(theta) * this.scale;
+        var y = this.radius * Math.sin(theta) * this.scale;
+        if (this.plane)
+            var step = this._vector2ToPlane(x, y, this.plane);
+        else 
+            var step = new IRIS.Vector2(x,y);
+        step.x += this.x;
+        step.y += this.y;
+        step.z += this.z;
+        if (IRIS._isFn(this.onStep)) //TODO: move this to parent.
+            this.onStep();
+        return step;
     }
 });
 
